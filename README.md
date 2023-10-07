47b031921b0a29767ab3a3e3617af8c8a7be9135
MeshachArinze <meshacharinze@gmail.com>
2023-10-07 18:24:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Tech Clarity Coach
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-5-orange.svg?style=flat-square)](#contributors-)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+## Quick Start
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## How to Contribute
+
+[Contributing to Tech Clarity Coach - Part One](https://www.youtube.com/watch?v=1Sx2rE8FdXM)
+
+[Contributing to Tech Clarity Coach - Part Two ](https://www.youtube.com/watch?v=LTseQoswT1Q)
+
+[Contributing to Tech Clarity Coach - Part Three](https://www.youtube.com/watch?v=_4e3p6XhYHI)
+
+## License
+
+This project is licensed under the MIT License.
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tbody>
+    <tr>
+      <td align="center" valign="top" width="14.28%"><a href="https://ebukanthony.netlify.app"><img src="https://avatars.githubusercontent.com/u/106475290?v=4?s=100" width="100px;" alt="ebuka anthony"/><br /><sub><b>ebuka anthony</b></sub></a><br /><a href="#content-ebuka1-anthony" title="Content">🖋</a> <a href="#ideas-ebuka1-anthony" title="Ideas, Planning, & Feedback">🤔</a> <a href="https://github.com/peterayeniofficial/techclaritycoach/commits?author=ebuka1-anthony" title="Code">💻</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/Creadify"><img src="https://avatars.githubusercontent.com/u/136258671?v=4?s=100" width="100px;" alt="Creadify"/><br /><sub><b>Creadify</b></sub></a><br /><a href="#design-Creadify" title="Design">🎨</a> <a href="#ideas-Creadify" title="Ideas, Planning, & Feedback">🤔</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/adeyemimichael"><img src="https://avatars.githubusercontent.com/u/83408171?v=4?s=100" width="100px;" alt="AKANDE Ayobami"/><br /><sub><b>AKANDE Ayobami</b></sub></a><br /><a href="#content-adeyemimichael" title="Content">🖋</a> <a href="#eventOrganizing-adeyemimichael" title="Event Organizing">📋</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://baraa-umber.vercel.app/"><img src="https://avatars.githubusercontent.com/u/87875062?v=4?s=100" width="100px;" alt="Baraa N. Mohsin"/><br /><sub><b>Baraa N. Mohsin</b></sub></a><br /><a href="https://github.com/peterayeniofficial/techclaritycoach/commits?author=BaraaNazar" title="Code">💻</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/DavOlufuwa"><img src="https://avatars.githubusercontent.com/u/87868500?v=4?s=100" width="100px;" alt="Dav.O."/><br /><sub><b>Dav.O.</b></sub></a><br /><a href="https://github.com/peterayeniofficial/techclaritycoach/commits?author=DavOlufuwa" title="Code">💻</a></td>
+    </tr>
+  </tbody>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
---
+++ data/heroes.ts
@@ -0,0 +1,162 @@
+export const heroes = [
+  {
+    name: "DHH",
+    description:
+      "Creator of Ruby on Rails, Co-owner & CTO of 37signals (Basecamp & HEY), NYT best-selling author, and Le Mans 24h class-winner.",
+    link: "https://twitter.com/dhh",
+    handle: "@dhh",
+    imageURL: "/assets/heroes/dhh.jpeg",
+  },
+  {
+    name: "Sarah Drasner",
+    description:
+      "Director of Eng, Web Infra @google , O'Reilly Author • OpenJS Board Member",
+    link: "https://twitter.com/sarah_edo",
+    handle: "@sarah_edo",
+    imageURL: "/assets/heroes/sarah.jpeg",
+  },
+  {
+    name: "Addy Osmani",
+    description:
+      "Engineering leader working on @GoogleChrome • Author • Great user & developer experiences",
+    link: "https://twitter.com/addyosmani",
+    handle: "@addyosmani",
+    imageURL: "/assets/heroes/addyosmani.jpeg",
+  },
+  {
+    name: "Ohans Emmanuel",
+    description:
+      "I do more than I tweet. 5x Author. Staff Software Engineer @HelloFresh Read my new book: http://ohans.me/ua-github",
+    link: "https://twitter.com/OhansEmmanuel",
+    handle: "@OhansEmmanuel",
+    imageURL: "/assets/heroes/ohans.jpeg",
+  },
+  {
+    name: "Lee Robinson",
+    description:
+      "Helping developers build a faster web. Teaching about web development, serverless, and React / Next.js.",
+    link: "https://twitter.com/leeerob",
+    handle: "@leeerob",
+    imageURL: "/assets/heroes/lee.jpeg",
+  },
+  {
+    name: "Franz Kati",
+    description:
+      "Udemy instructor with 75k+ Students. Teaching about building a successful career in tech, with emphasis on growth and standing out as a software engineer",
+    link: "https://twitter.com/bahdcoder",
+    handle: "@bahdcoder",
+    imageURL: "/assets/heroes/Franz.jpeg",
+  },
+  {
+    name: "Sara Soueidan", 
+    description:
+      "Inclusive UI/design engineer, int'l speaker, author, trainer. • Creator of the Practical Accessibility course http://practical-accessibility.today ",
+    link: "https://twitter.com/SaraSoueidan",
+    handle: "@SaraSoueidan",
+    imageURL: "/assets/heroes/Sara.jpeg",
+  },
+  {
+    name: "Matteo Collina",
+    description:
+      "@platformatic Co-Founder & CTO, @nodejs TSC member, Lead maintainer @fastifyjs , Board @OpenJSF , Conference Speaker, Ph.D.",
+    link: "https://twitter.com/matteocollina",
+    handle: "@matteocollina",
+    imageURL: "/assets/heroes/matteocollina.jpeg",
+  },
+  {
+    name: "Veni Kunche",
+    description: "Founder. Maker. Software Engineer. I run @DiversifyTechCo.",
+    link: "https://twitter.com/venikunche",
+    handle: "@venikunche",
+    imageURL: "/assets/heroes/veni.jpeg",
+  },
+  {
+    name: "Andrei Neagoie",
+    description:
+      "run the best tech school in the world that gets people (actually) hired at top companies (1,000,000+ pros have taken a ZTM course) ",
+    link: "https://twitter.com/AndreiNeagoie",
+    handle: "@AndreiNeagoie",
+    imageURL: "/assets/heroes/andrei.jpeg",
+  },
+  {
+    name: "Sebastian Sigl",
+    description:
+      " Passionate software engineer @Adevinta, Building websites in public, Tweeting about backend & frontend, Free coaching, Ex @eBay with 8 years work experience",
+    link: "https://twitter.com/sesigl",
+    handle: "@sesigl",
+    imageURL: "/assets/heroes/seb.jpeg",
+  },
+  {
+    name: "Chris Do",
+    description:
+      "Bilingual creative who lives at the intersection of business & design. Content creator, public speaker, workshopper. Host of top 20 Futur podcast.",
+    link: "https://twitter.com/theChrisDo",
+    handle: "@theChrisdo",
+    imageURL: "/assets/heroes/chris-do.jpeg",
+  },
+  {
+    name: "Nader Dabit",
+    description:
+      "Director of DevRel @aaveaave 👻 @lensprotocol 🌿 // founder @developer_dao // experimenting @arweaveeco // React* web3 and AI",
+    link: "https://twitter.com/dabit3",
+    handle: "@dabit3",
+    imageURL: "/assets/heroes/nader-dabit.png",
+  },
+  {
+    name: "Shaundai Person",
+    description:
+      "Sr Software Engineer @Netflix • Building TypeScript for JavaScript Devs Course (http://tsforjs.com) • Bluesky: @ http://shaundai.com",
+    link: "https://twitter.com/shaundai",
+    handle: "@shaundai",
+    imageURL: "/assets/heroes/shaundai.jpeg",
+  },
+  {
+    name: "Peter Ayeni",
+    description:
+      "I change the world, by helping people get started in tech and social entrepenurship",
+    link: "https://twitter.com/thepeterayeni",
+    handle: "@thepeterayeni",
+    imageURL: "/assets/heroes/peter-ayeni.jpeg",
+  },
+   {
+    name: "Favor Onuoha",
+    description:
+      "DevRel + Growth Lead ShowwcaseHQ, Technical Writer, Blockchain developer Content Creator, JavaScript | Helping people break into web3!",
+    link: "https://twitter.com/heyOnuoha",
+    handle: "@heyOnuoha",
+    imageURL: "/assets/heroes/favor.jpeg",
+  },
+  {
+    name: "Captain-EO",
+    description:
+      "Software Engineer • Intro tech bro •",
+    link: "https://twitter.com/eo_eboh",
+    handle: "@eo_eboh",
+    imageURL: "/assets/heroes/eboh.jpeg",
+  }, 
+   {
+    name: "chinaa",
+    description:
+      "Solo Design Agency - Creating bold and awesome brands to: Comm. Mgr @dreamaxhq , Comm. Adv. @fof_accra",
+    link: "https://twitter.com/Yaa_gurl",
+    handle: "@Yaa_gurl",
+    imageURL: "/assets/heroes/chinaa.jpeg",
+  }, 
+   {
+    name: "Dion Hinchcliffe",
+    description:
+      "Thinker, strategist, enterprise architect, keynote speaker, analyst, book author, futurist on IT,",
+    link: "https://twitter.com/dhinchcliffe",
+    handle: "@dhinchcliffe",
+    imageURL: "/assets/heroes/dion.jpeg",
+  }, 
+  {
+    name: "Nando de Freitas",
+    description:
+      "I research intelligence to understand what we are, and to harness it wisely. Research Director DeepMind",
+    link: "https://twitter.com/NandoDF",
+    handle: "@NandoDF",
+    imageURL: "/assets/heroes/nando.jpeg",
+  },
+];
+
